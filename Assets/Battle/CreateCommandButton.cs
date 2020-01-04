8fa77afd0468e6550fcab93c2e864761b8c18cba
mizunoawa.web <saeki_takatoshi@outlook.jp>
2020-01-05 00:16:16 +0900

    コマンドメニューとビルド設定

---
+++ Assets/Battle/CreateCommandButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateCommandButton : MonoBehaviour
+{
+    public GameObject ButtonPrefab;
+
+    private IList<GameObject> _buttons = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        GameObject button;
+        for (int i = 0; i < 4; i++)
+        {
+            button = Instantiate(ButtonPrefab, transform);
+            button.transform.Find("Name").GetComponent<Text>().text = $"スキル{i}";
+            _buttons.Add(button);
+        }
+
+        button = Instantiate(ButtonPrefab, transform);
+        button.transform.Find("Name").GetComponent<Text>().text = "交代";
+        button.transform.Find("Value").GetComponent<Text>().text = "∞";
+        _buttons.Add(button);
+    }
+
+    private void OnDisable()
+    {
+        foreach (var button in _buttons)
+        {
+            Destroy(button);
+        }
+        _buttons.Clear();
+    }
+}
