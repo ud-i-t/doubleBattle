b3da03b17c7fec72b758196d019550ebafef300e
mizunoawa.web <saeki_takatoshi@outlook.jp>
2020-01-05 21:32:26 +0900

    ActorとEnemyのVM追加

---
+++ Assets/ViewModel/ActorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorViewModel : MonoBehaviour
+{
+    public Actor Actor;
+    public MasterActorData MasterData;
+
+    public void Awake()
+    {
+        Actor = new Actor();
+    }
+}
---
+++ Assets/ViewModel/EnemyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyViewModel : MonoBehaviour
+{
+    public Enemy Enemy;
+    public MasterEnemyData MasterData;
+
+    public void Awake()
+    {
+        Enemy = new Enemy();
+    }
+}
