105f7182dd52035bd226b8af1d91d7d9564c64c2
mizunoawa.web <saeki_takatoshi@outlook.jp>
2020-01-07 21:00:33 +0900

    リポジトリ追加

---
+++ Assets/Model/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repository
+{
+    private static Dictionary<Type, object> _keyValuePairs = new Dictionary<Type, object>();
+
+    public static T Get<T>(){
+        return (T)_keyValuePairs[typeof(T)];
+    }
+
+    public static void Set<T>(T obj)
+    {
+        _keyValuePairs.Add(typeof(T), obj);
+    }
+}
